969f5247f7ec9f3f3f94ea05fed95e0d53f9564f
E.Karadzhov <e.karadzhov@double-d-it.de>
2025-01-07 14:38:12 +0100

    Introduced shadcn breadcrumb instead custom

--- app/(landing-pages)/contact/page.tsx
+++ app/(landing-pages)/contact/page.tsx
@@ -1,11 +1,14 @@
-import { breadcrumbContactPage } from "@/data/breadcrumb";
-import Breadcrumb from "@/components/breadcrumb";
+import type { Metadata } from "next";
+import TitleWithBreadcrumb from "@/components/title-with-breadcrumb";
 import Divider from "@/components/divider";
+import { contactPage } from "@/data/pages";
+
+export const metadata: Metadata = contactPage.metadata;
 
 const Contact = () => {
   return (
     <>
-      <Breadcrumb item={breadcrumbContactPage} />
+      <TitleWithBreadcrumb item={contactPage} />
       <Divider />
     </>
   );
