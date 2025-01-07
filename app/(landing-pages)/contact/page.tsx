47d14dfaaf1425d49e29f4a036481da78f8ce49a
E.Karadzhov <e.karadzhov@double-d-it.de>
2025-01-07 22:23:10 +0100

    Implemented contact form & company contact information

--- app/(landing-pages)/contact/page.tsx
+++ app/(landing-pages)/contact/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import TitleWithBreadcrumb from "@/components/title-with-breadcrumb";
 import Divider from "@/components/divider";
 import { contactPage } from "@/data/pages";
+import ContactForm from "@/app/(landing-pages)/contact/_components/contact-form";
 
 export const metadata: Metadata = contactPage.metadata;
 
@@ -9,6 +10,9 @@ const Contact = () => {
   return (
     <>
       <TitleWithBreadcrumb item={contactPage} />
+      {/*<Divider />*/}
+
+      <ContactForm />
       <Divider />
     </>
   );
